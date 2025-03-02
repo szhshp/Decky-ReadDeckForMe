db8ee2d0a31a5a3a10616a37dfbb1a00bd126f83
Szhshp <24031shp@sina.com>
2025-03-02 22:35:49 +0800

    fix: update deploy script to improve logging and set permissions for decky-rifm

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,17 +8,20 @@ if test -e $HOME/homebrew/plugins/decky-rifm; then
     echo "LOG: Removed existing decky-rifm."
 fi
 
-# Comment this deletion if on debug mode
-echo "LOG: Checking if /tmp/decky-rifm.zip exists..."
-if test -e /tmp/decky-rifm.zip; then
-    echo "LOG: Removing /tmp/decky-rifm.zip..."
-    sudo rm /tmp/decky-rifm.zip
-    echo "LOG: Removed /tmp/decky-rifm.zip."
-fi
+# # Comment this deletion if on debug mode
+# echo "LOG: Checking if /tmp/decky-rifm.zip exists..."
+# if test -e /tmp/decky-rifm.zip; then
+#     echo "LOG: Removing /tmp/decky-rifm.zip..."
+#     sudo rm /tmp/decky-rifm.zip
+#     echo "LOG: Removed /tmp/decky-rifm.zip."
+# fi
 
-echo "LOG: Downloading decky-rifm..."
-curl -L https://gh-proxy.com/github.com/szhshp/read-deck-for-me/releases/download/latest/decky-rifm.zip > /tmp/decky-rifm.zip
-echo "LOG: Downloaded decky-rifm."
+# echo "LOG: Downloading decky-rifm..."
+# curl -L https://ghproxy.net/https://github.com/szhshp/read-deck-for-me/releases/download/latest/decky-rifm.zip > /tmp/decky-rifm.zip
+# echo "LOG: Downloaded decky-rifm."
+
+
+# ------------------- PREQUISITES CHECK FINISHED -------------------
 
 echo "LOG: Checking if plugins directory exists..."
 if test ! -e $HOME/homebrew/plugins; then
@@ -27,10 +30,15 @@ if test ! -e $HOME/homebrew/plugins; then
     echo "LOG: Created plugins directory."
 fi
 
+
 echo "LOG: Stopping plugin_loader service..."
 systemctl --user stop plugin_loader 2> /dev/null
 sudo systemctl stop plugin_loader 2> /dev/null
 echo "LOG: Stopped plugin_loader service."
+
+echo "LOG: Setting permissions for decky-rifm..."
+sudo chmod -R 777 $HOME/homebrew/plugins/decky-rifm
+echo "LOG: Permissions set."
 
 echo "LOG: Unzipping decky-rifm.zip..."
 sudo unzip -qq /tmp/decky-rifm.zip -d $HOME/homebrew/plugins/
@@ -51,5 +59,9 @@ echo "LOG: Removed /tmp/decky-rifm.zip."
 echo "LOG: Starting plugin_loader service..."
 sudo systemctl start plugin_loader
 echo "LOG: Started plugin_loader service."
+
+echo "LOG: Setting permissions for decky-rifm..."
+sudo chmod -R 777 $HOME/homebrew/plugins/decky-rifm
+echo "LOG: Permissions set."
 
 echo "LOG: DECKY-RIFM IS INSTALLED."
